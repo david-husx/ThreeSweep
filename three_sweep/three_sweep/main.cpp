0310d836dd4307e3713e5291faf8d9aee88b9543
ThinkGo <david_hsx@hotmail.com>
2014-04-01 18:22:46 +0800

    opencv2.4.4

--- three_sweep/three_sweep/main.cpp
+++ three_sweep/three_sweep/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "boundary.h"
+//#include "CurveExtraction\CurveExtractionStdafx.h"
+#include "CurveExtraction\CmCurveEx.h"
 
 using namespace std;
 
@@ -7,6 +9,9 @@ int main()
 {
 	Boundary b;
 	b.test_getSegmentIntersection();
+
+	//string cImgFile = "data/102943_curve.png";
+	//CmCurveEx::edge2vector(cImgFile, true);
 
 	system("pause");
 	return 0;
